120dbb20027c14498f18d6e239e6376611ef2006
yan fei <yan00126@algonquinlive.com>
2025-06-12 23:56:29 -0400

    Navbar and Hero component added

---
+++ src/components/Hero.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import heroImg from "../assets/hero.svg";
+import { FaGithubSquare, FaLinkedin, FaTwitterSquare } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
+const Hero = () => {
+  return (
+    <div className="bg-emerald-100 py-24">
+      <div className="mx-auto max-w-7xl px-8 grid md:grid-cols-2 items-center gap-8">
+        <article>
+          <h1 className="text-7xl font-bold tracking-wider">I'm Fei</h1>
+          <p className="mt-4 text-3xl text-slate-700 capitalize tracking-wide">
+            Full Stack Web Developer
+          </p>
+          <p className="mt-2 text-lg text-slate-700 capitalize tracking-wide">
+            turning ideas into interactive reality
+          </p>
+          <div className="flex gap-x-4 mt-4">
+            <a href="https://github.com/yan00126">
+              <FaGithubSquare className="h-8 w-8 text-slate-500 hover:text-black duration-300" />
+            </a>
+            <a href="https://www.linkedin.com/in/fei-yan-86331a45/">
+              <FaLinkedin className="h-8 w-8 text-slate-500 hover:text-black duration-300" />
+            </a>
+            <a href="https://x.com/felixPEKING">
+              <FaSquareXTwitter className="h-8 w-8 text-slate-500 hover:text-black duration-300" />
+            </a>
+          </div>
+        </article>
+        <article className="hidden md:block">
+          <img src={heroImg} className="h-80 lg:h-96" />
+        </article>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
