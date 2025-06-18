1df36d9d403015249f42a824f0d1d07f22f0d6ff
yan fei <yan00126@algonquinlive.com>
2025-06-17 23:54:58 -0400

    change a typo

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -6,7 +6,7 @@ import { skills } from "../data";
 const Skills = () => {
   return (
     <section className="py-20 align-element " id="skills">
-      <SectionTitle text="text stack" />
+      <SectionTitle text="tech stack" />
       <div className="py-16 grid gap-8 md:grid-cols-2 lg:grid-cols-3">
         {skills.map((skill) => {
           return <SkillsCard key={skill.id} {...skill} />;
