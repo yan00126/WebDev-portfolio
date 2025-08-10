de45d7e7595f46ffd35d6f4c91bd018a9de84d43
yan fei <yan00126@algonquinlive.com>
2025-08-10 14:53:22 -0400

    add hotjar

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import Hotjar from "@hotjar/browser";
 
-createRoot(document.getElementById('root')).render(
+const siteId = 6489320;
+const hotjarVersion = 6;
+
+Hotjar.init(siteId, hotjarVersion);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
